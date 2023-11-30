e9ff9c358fddd5a8978f0fd9caa49656fb2d824d
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2023-11-30 20:31:28 +0530

    Update revokecoupon.js

--- commands/revokecoupon.js
+++ commands/revokecoupon.js
@@ -19,6 +19,10 @@ module.exports = {
     const code = interaction.options.getString('code');
 
     try {
+      const member = interaction.guild.members.cache.get(interaction.user.id);
+      if (!member.roles.cache.has(config.role_id)) {
+        return interaction.reply('You do not have the required role to use this command.');
+      }
       const response = await fetch(`${config.url}/api/coupons/revoke/${code}`, {
         method: 'GET',
         headers: {
