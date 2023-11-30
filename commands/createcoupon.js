62f551f9ff472e89b4b0acad515feb2566a433f7
Nightmaregodss <81895400+Nightmaregodss@users.noreply.github.com>
2023-11-30 20:31:50 +0530

    Update createcoupon.js

--- commands/createcoupon.js
+++ commands/createcoupon.js
@@ -82,6 +82,10 @@ module.exports = {
     const uses = interaction.options.getInteger('uses');
 
     try {
+      const member = interaction.guild.members.cache.get(interaction.user.id);
+      if (!member.roles.cache.has(config.role_id)) {
+        return interaction.reply('You do not have the required role to use this command.');
+      }
       const response = await fetch(`${config.url}/api/coupons/create`, {
         method: 'POST',
         headers: {
